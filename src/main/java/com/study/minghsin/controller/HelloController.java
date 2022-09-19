3b9d7ff99fdd99a1cb5f03d99e7975fb494419d0
WANG MINGXIN <minghsin@foxmail.com>
2022-09-19 20:01:28 +0800

    [0919]增加日志依赖

--- src/main/java/com/study/minghsin/controller/HelloController.java
+++ src/main/java/com/study/minghsin/controller/HelloController.java
@@ -1,15 +1,18 @@
 package com.study.minghsin.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/com")
+@Slf4j
 public class HelloController {
 
     @PostMapping("/hello")
     public String hello(){
+        log.info("now a request is coming");
         return "hello from springSolution";
     }
 
